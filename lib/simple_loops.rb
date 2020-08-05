a7e2f76c09bd594383eefc67b3c7c5c58bdcddb6
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 00:52:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,13 +8,7 @@ def loop_message_five_times(message)
   end
 end 
 
-def loop_message_five_times(message,n)
-  counter=0 
-  while counter<n do
-    puts message
-    counter +=1 
-  end 
-end
+
 
 def output_array(array)
   counter=0 
