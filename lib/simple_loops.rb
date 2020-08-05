147f8f79f31c6c7ab88f061d3944f1a867244699
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 01:01:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,7 +28,7 @@ def return_string_array(array)
   counter=0 
   new_array=[]
   while counter<array.length do
-    new_array.push(array[count].to_s)
+    new_array.push(array[counter].to_s)
     counter+=1 
   end
   new_array
